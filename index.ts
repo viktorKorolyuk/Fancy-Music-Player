294aac213d4125999fbaf330a2f6df61b58d44b6
Viktor Korolyuk <viktor.korolyuk@gmail.com>
2019-07-31 14:04:17 -0400

    Ported index.js into index.ts

---
+++ index.ts
@@ -0,0 +1,38 @@
+const {app, BrowserWindow} = require("electron");
+const url = require("url");
+const path = require("path");
+
+// Enables hot reloading
+// require('electron-reload')(__dirname);
+
+var win;
+
+function createWindow(){
+
+    win = new BrowserWindow({
+        width: 1920/2,
+        height: 1080/2,
+        icon: path.join(__dirname, "assets/icons/png/64x64.png"),
+        frame:false,
+    });
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, "index.html"),
+        protocol:"file:",
+        slashes:true
+    }));
+
+    // Turn off the menubar at the top.
+    win.setMenu(null);
+
+
+//   win.webContents.openDevTools();
+}
+
+app.on("ready", () => {
+    createWindow();
+});
+
+app.on("window-all-closed", () => {
+    app.quit();
+});

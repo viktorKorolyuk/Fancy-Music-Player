1bc68249d92bd618ed2f9d6125c9cf22e2646004
Viktor Korolyuk <8334048+viktorKorolyuk@users.noreply.github.com>
2021-07-11 10:00:53 -0400

    Update file drop-zone to fill the application screen

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -4,16 +4,15 @@ import * as htmlContent from './index.html'
 import './FileDrag'
 import './Overlay'
 import './AudioPlayer'
-import { $ } from './Helper';
+
 import { dragover_handler, drop_handler } from './FileDrag';
 import { setup_player } from './AudioPlayer';
 
 let app = document.getElementById("app") as HTMLElement
 app.innerHTML = htmlContent.default
 
-let playlist = $("#playlist") as HTMLElement
-playlist.addEventListener("dragover", (ev) => dragover_handler(ev))
-playlist.addEventListener("drop", async (ev) => await drop_handler(ev))
+app.addEventListener("dragover", (ev) => dragover_handler(ev))
+app.addEventListener("drop", async (ev) => await drop_handler(ev))
 
 setup_player()
 // Problem: AudioPlayer requests playlist before it is ready
